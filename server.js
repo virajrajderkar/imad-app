9d616b3527f7149dc6b307a433c162d659034e01
virajrajderkar <30744729+virajrajderkar@users.noreply.github.com>
2017-09-04 17:23:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-  var articleName = reg.params.articleName;
+  var articleName = res.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
