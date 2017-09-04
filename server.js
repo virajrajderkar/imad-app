1a57437c9359cbcc5317ea4d98ec41b98bafeaeb
virajrajderkar <30744729+virajrajderkar@users.noreply.github.com>
2017-09-04 17:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var articles = {
 };
 
 function createTemplate(data){
-   // var title = data.title;
+    var title = data.title;
     var head = data.head;
     var p = data.p;
     
@@ -40,12 +40,12 @@ function createTemplate(data){
            
             <head>
                 <link href="/ui/style.css" rel="stylesheet" />
-              //  <title> $title </title>
+                <title>$title</title>
                
             </head>
             <body>
-                <h3> $head </h3>
-               <p> $p  </p>
+                <h3>$head</h3>
+               <p>$p</p>
             </body>
         </html>`;
 }
